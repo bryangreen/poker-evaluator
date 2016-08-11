89cd0cdef4ba1a69bebec10616cbe07e1dadc7a8
Bryan Green <mail@bryangreen.me>
2016-08-11 10:42:39 -0600

    Updating dependencies.

--- src/main.browser.ts
+++ src/main.browser.ts
@@ -1,3 +1,5 @@
+
+
 import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {HTTP_PROVIDERS} from '@angular/http';

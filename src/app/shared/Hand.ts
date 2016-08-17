c2c4fb537df5ecc53f4ffb11c7df85e8a189dab6
Bryan Green <mail@bryangreen.me>
2016-08-17 08:05:53 -0600

    Refactoring hand evaluators and some code formatting.

--- src/app/shared/Hand.ts
+++ src/app/shared/Hand.ts
@@ -4,7 +4,6 @@ export class Hand {
 
   cards: Array<Card> = [];
 
-
   constructor() {
 
   }
--- src/app/shared/evaluator/CardRankSequenceEvaluator.ts
+++ src/app/shared/evaluator/CardRankSequenceEvaluator.ts
@@ -9,21 +9,30 @@ export class CardRankSequenceEvaluator {
   }
 
   isRankInSequence(hand: Hand): boolean {
-    let sorter = new CardSorter(),
-      sortedCards = sorter.sortAsc(hand.getCards());
+    let sortedCards: Array<Card> = this.sort(hand),
+      orderedRanks: Array<number> = this.orderedRanks(sortedCards),
+      firstRankInOrder: number = orderedRanks[0];
 
+    return this.isSequentialOrder(orderedRanks, firstRankInOrder);
+  }
+
+  isSequentialOrder(orderedRanks: Array<number>, firstRankInOrder: number): boolean {
+    // Determine if every rank is in sequential order.
+    return orderedRanks.every((value: number, index: number): boolean=> {
+      return (value == index + firstRankInOrder) ? true : false;
+    });
+  }
+
+  orderedRanks(sortedCards: Array<Card>): Array<number> {
     // Return a result with the ranks in order.
-    let orderedRanks = sortedCards.reduce(function (result: Array<number>, card: Card): Array<number> {
+    return sortedCards.reduce(function (result: Array<number>, card: Card): Array<number> {
       result.push(card.rank.ranking);
       return result;
     }, Array<number>());
+  }
 
-    let start: number = orderedRanks[0];
-
-    // Determine if every rank is in sequential order.
-    return orderedRanks.every((value: number, index: number): boolean=> {
-      return (value == index + start) ? true : false;
-    });
+  sort(hand: Hand): Array<Card> {
+    return new CardSorter().sortAsc(hand.getCards());
   }
 
 }

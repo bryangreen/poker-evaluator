db12106948370af8684c01c0b15ed24e3d8e6d89
Bryan Green <mail@bryangreen.me>
2016-08-17 12:06:06 -0600

    Removed HTTP for now.

--- src/main.browser.ts
+++ src/main.browser.ts
@@ -1,6 +1,5 @@
 import {LocationStrategy, HashLocationStrategy} from "@angular/common";
 import {bootstrap} from "@angular/platform-browser-dynamic";
-import {HTTP_PROVIDERS} from "@angular/http";
 import {APP_ROUTER_PROVIDERS} from "./app/app.routes";
 import {App} from "./app/app";
 import {GameService} from "./app/GameService";
@@ -9,7 +8,6 @@ import {GameService} from "./app/GameService";
 
 bootstrap(App, [
   GameService,
-  HTTP_PROVIDERS,
   APP_ROUTER_PROVIDERS,
   {provide: LocationStrategy, useClass: HashLocationStrategy}
 ]).catch(err => console.error(err));

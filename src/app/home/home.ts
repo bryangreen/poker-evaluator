2ed3dddc568ad12fd4d95fd05d528e11f4ad0407
Bryan Green <mail@bryangreen.me>
2016-08-17 09:10:32 -0600

    Cleaning up hand display and ui.

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,8 +1,5 @@
 import {Component} from "@angular/core";
 import {Evaluator} from "../shared/evaluator/Evaluator";
-import {Card} from "../shared/Card";
-import {Suit, StandardSuits} from "../shared/Suit";
-import {Rank, StandardRanks} from "../shared/Rank";
 import {GameModel} from "../GameModel";
 import {GameService} from "../GameService";
 
@@ -19,34 +16,25 @@ export class Home {
   gameModel: GameModel = new GameModel();
 
   constructor(public gameService: GameService) {
+
   }
 
-
-  shuffle() {
+  deck() {
     this.gameService.dealer.start();
-  }
-
-  deal() {
-    this.gameService.dealer.addCard(new Card(Rank.createFromIndex(StandardRanks.Ten), Suit.createFromIndex(StandardSuits.Club)));
-    this.gameService.dealer.addCard(new Card(Rank.createFromIndex(StandardRanks.Jack), Suit.createFromIndex(StandardSuits.Club)));
-    this.gameService.dealer.addCard(new Card(Rank.createFromIndex(StandardRanks.Queen), Suit.createFromIndex(StandardSuits.Club)));
-    this.gameService.dealer.addCard(new Card(Rank.createFromIndex(StandardRanks.King), Suit.createFromIndex(StandardSuits.Heart)));
-    this.gameService.dealer.addCard(new Card(Rank.createFromIndex(StandardRanks.Nine), Suit.createFromIndex(StandardSuits.Club)));
   }
 
   evaluate() {
     let evaluator = new Evaluator();
-
     this.gameService.winningHand = evaluator.evaluate(this.gameService.hand);
   }
 
-  dealEvaluate() {
-    console.clear();
-
+  hand() {
     let hand = this.gameService.dealer.deal();
-    console.log(hand.toString());
     this.gameService.hand = hand;
+  }
 
+  newHandEvaluate() {
+    this.hand();
     this.evaluate();
   }
 

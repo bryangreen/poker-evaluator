795587a902f5cc6274e462952520135c537f9a98
Bryan Green <mail@bryangreen.me>
2016-08-17 18:14:17 -0600

    Adding the ability to select individual cards to add to the hand.

--- src/app/manual/manual.ts
+++ src/app/manual/manual.ts
@@ -1,7 +1,7 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {Card} from "../shared/Card";
-import {Suit, StandardSuits} from "../shared/Suit";
-import {Rank, StandardRanks} from "../shared/Rank";
+import {Suit, StandardSuits, SuitInterface} from "../shared/Suit";
+import {Rank, StandardRanks, RankInterface} from "../shared/Rank";
 import {GameStore} from "../services/GameStore";
 import {Deck} from "../shared/Deck";
 
@@ -13,106 +13,127 @@ import {Deck} from "../shared/Deck";
   styleUrls: ['./manual.css'],
   templateUrl: './manual.html'
 })
-export class Manual {
+export class Manual implements OnInit {
+  cards: Array<Card>;
 
   constructor(public gameStore: GameStore) {
   }
 
+  ngOnInit(): any {
+    this.cards = [];
+    for (let suit of Suit.suits) {
+      for (let rank of Rank.ranks) {
+        this.cards.push(new Card(Rank.build(rank), Suit.build(suit)));
+      }
+    }
+  }
+
+  getGameStore(): GameStore {
+    return this.gameStore;
+  }
+
+  addCard(card: Card) {
+    if (!this.getGameStore().deck) {
+      this.getGameStore().deck = new Deck();
+    }
+    this.getGameStore().deck.add(card);
+  }
+
   ensureDeckExists() {
-    if (!this.gameStore.deck) {
-      this.gameStore.deck = new Deck();
-      this.gameStore.deck.create();
+    if (!this.getGameStore().deck) {
+      this.getGameStore().deck = new Deck();
+      this.getGameStore().deck.create();
     }
   }
 
   royalFlush() {
     this.ensureDeckExists();
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Ten), Suit.createFromIndex(StandardSuits.Club)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Jack), Suit.createFromIndex(StandardSuits.Club)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Queen), Suit.createFromIndex(StandardSuits.Club)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.King), Suit.createFromIndex(StandardSuits.Club)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Ace), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Ten), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Jack), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Queen), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.King), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Ace), Suit.createFromIndex(StandardSuits.Club)));
   }
 
   straightFlush() {
     this.ensureDeckExists();
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Ten), Suit.createFromIndex(StandardSuits.Club)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Jack), Suit.createFromIndex(StandardSuits.Club)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Queen), Suit.createFromIndex(StandardSuits.Club)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.King), Suit.createFromIndex(StandardSuits.Club)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Nine), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Ten), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Jack), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Queen), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.King), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Nine), Suit.createFromIndex(StandardSuits.Club)));
   }
 
   fourOfAKind() {
     this.ensureDeckExists();
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Ten), Suit.createFromIndex(StandardSuits.Diamond)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Ten), Suit.createFromIndex(StandardSuits.Club)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Ten), Suit.createFromIndex(StandardSuits.Club)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Ten), Suit.createFromIndex(StandardSuits.Heart)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Nine), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Ten), Suit.createFromIndex(StandardSuits.Diamond)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Ten), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Ten), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Ten), Suit.createFromIndex(StandardSuits.Heart)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Nine), Suit.createFromIndex(StandardSuits.Club)));
   }
 
   fullHouse() {
     this.ensureDeckExists();
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Two), Suit.createFromIndex(StandardSuits.Diamond)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Two), Suit.createFromIndex(StandardSuits.Club)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Two), Suit.createFromIndex(StandardSuits.Spade)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Queen), Suit.createFromIndex(StandardSuits.Heart)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Queen), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Two), Suit.createFromIndex(StandardSuits.Diamond)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Two), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Two), Suit.createFromIndex(StandardSuits.Spade)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Queen), Suit.createFromIndex(StandardSuits.Heart)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Queen), Suit.createFromIndex(StandardSuits.Club)));
   }
 
   flush() {
     this.ensureDeckExists();
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Four), Suit.createFromIndex(StandardSuits.Club)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Four), Suit.createFromIndex(StandardSuits.Club)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Four), Suit.createFromIndex(StandardSuits.Club)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Four), Suit.createFromIndex(StandardSuits.Club)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Four), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Four), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Four), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Four), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Four), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Four), Suit.createFromIndex(StandardSuits.Club)));
   }
 
   straight() {
     this.ensureDeckExists();
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Five), Suit.createFromIndex(StandardSuits.Spade)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Six), Suit.createFromIndex(StandardSuits.Club)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Seven), Suit.createFromIndex(StandardSuits.Spade)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Eight), Suit.createFromIndex(StandardSuits.Heart)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Nine), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Five), Suit.createFromIndex(StandardSuits.Spade)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Six), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Seven), Suit.createFromIndex(StandardSuits.Spade)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Eight), Suit.createFromIndex(StandardSuits.Heart)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Nine), Suit.createFromIndex(StandardSuits.Club)));
   }
 
   threeOfAKind() {
     this.ensureDeckExists();
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Eight), Suit.createFromIndex(StandardSuits.Spade)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Eight), Suit.createFromIndex(StandardSuits.Club)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Eight), Suit.createFromIndex(StandardSuits.Heart)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Two), Suit.createFromIndex(StandardSuits.Heart)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Ace), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Eight), Suit.createFromIndex(StandardSuits.Spade)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Eight), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Eight), Suit.createFromIndex(StandardSuits.Heart)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Two), Suit.createFromIndex(StandardSuits.Heart)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Ace), Suit.createFromIndex(StandardSuits.Club)));
   }
 
   twoPair() {
     this.ensureDeckExists();
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Jack), Suit.createFromIndex(StandardSuits.Spade)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Jack), Suit.createFromIndex(StandardSuits.Club)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Four), Suit.createFromIndex(StandardSuits.Heart)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Four), Suit.createFromIndex(StandardSuits.Spade)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Ace), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Jack), Suit.createFromIndex(StandardSuits.Spade)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Jack), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Four), Suit.createFromIndex(StandardSuits.Heart)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Four), Suit.createFromIndex(StandardSuits.Spade)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Ace), Suit.createFromIndex(StandardSuits.Club)));
   }
 
   pair() {
     this.ensureDeckExists();
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Queen), Suit.createFromIndex(StandardSuits.Spade)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Queen), Suit.createFromIndex(StandardSuits.Club)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Two), Suit.createFromIndex(StandardSuits.Heart)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Four), Suit.createFromIndex(StandardSuits.Spade)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Ace), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Queen), Suit.createFromIndex(StandardSuits.Spade)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Queen), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Two), Suit.createFromIndex(StandardSuits.Heart)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Four), Suit.createFromIndex(StandardSuits.Spade)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Ace), Suit.createFromIndex(StandardSuits.Club)));
   }
 
   highCard() {
     this.ensureDeckExists();
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Jack), Suit.createFromIndex(StandardSuits.Spade)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Four), Suit.createFromIndex(StandardSuits.Club)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Six), Suit.createFromIndex(StandardSuits.Heart)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Eight), Suit.createFromIndex(StandardSuits.Spade)));
-    this.gameStore.deck.add(new Card(Rank.createFromIndex(StandardRanks.Ace), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Jack), Suit.createFromIndex(StandardSuits.Spade)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Four), Suit.createFromIndex(StandardSuits.Club)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Six), Suit.createFromIndex(StandardSuits.Heart)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Eight), Suit.createFromIndex(StandardSuits.Spade)));
+    this.getGameStore().deck.add(new Card(Rank.createFromIndex(StandardRanks.Ace), Suit.createFromIndex(StandardSuits.Club)));
   }
 
 }

85d0a81a5386546a031b0b196b16309ee11ec788
Bryan Green <mail@bryangreen.me>
2016-08-17 19:52:15 -0600

    Adding typings.

---
+++ typings/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="globals/chai/index.d.ts" />
+/// <reference path="globals/core-js/index.d.ts" />
+/// <reference path="globals/mocha/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/sinon/index.d.ts" />
+/// <reference path="modules/lodash/index.d.ts" />
